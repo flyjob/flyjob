488f3334b17bd1054a03bb1cf0bd9ce158dae429
KamilAliyev1 <kamilaliyev240@gmail.com>
2024-04-14 14:01:16 +0400

    h

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/query/concretes/ExperienceQueryHandler.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/query/concretes/ExperienceQueryHandler.java
@@ -1,34 +1,71 @@
 package az.rock.flyjob.js.domain.presentation.handler.query.concretes;
 
+import az.rock.flyjob.js.domain.core.exception.course.CourseNotFoundException;
+import az.rock.flyjob.js.domain.presentation.dto.criteria.CourseCriteria;
+import az.rock.flyjob.js.domain.presentation.dto.criteria.ExperienceCriteria;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.course.AnyCourseResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.course.MyCourseResponseModel;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.experience.AnyExperienceResponseModel;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.experience.MyExperienceResponseModel;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.experience.simple.SimpleAnyExperienceResponseModel;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.experience.simple.SimpleMyExperienceResponseModel;
 import az.rock.flyjob.js.domain.presentation.handler.query.abstracts.AbstractExperienceQueryHandler;
+import az.rock.flyjob.js.domain.presentation.ports.output.repository.query.AbstractExperienceQueryRepositoryAdapter;
 import az.rock.flyjob.js.domain.presentation.security.AbstractSecurityContextHolder;
+import az.rock.lib.domain.id.js.CourseID;
+import az.rock.lib.domain.id.js.ResumeID;
+import az.rock.lib.valueObject.AccessModifier;
 import az.rock.lib.valueObject.SimplePageableRequest;
 import az.rock.lib.valueObject.SimplePageableResponse;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class ExperienceQueryHandler implements AbstractExperienceQueryHandler {
     private final AbstractSecurityContextHolder securityContextHolder;
 
+    private final AbstractExperienceQueryRepositoryAdapter abstractExperienceQueryHandler;
+
+    private List<AccessModifier> mockAccessModifiers = List.of(AccessModifier.values());
+
+    public ExperienceQueryHandler(AbstractSecurityContextHolder securityContextHolder, AbstractExperienceQueryRepositoryAdapter abstractExperienceQueryHandler) {
+        this.securityContextHolder = securityContextHolder;
+        this.abstractExperienceQueryHandler = abstractExperienceQueryHandler;
+    }
+
     @Override
     public SimplePageableResponse<MyExperienceResponseModel> allMyExperiences(SimplePageableRequest pageableRequest) {
-        return null;
+        var resumeId = securityContextHolder.availableResumeID();
+        var criteria = ExperienceCriteria.Builder.builder().resumeID(resumeId).build();
+        var experiences = abstractExperienceQueryHandler.fetchAllExperiences(criteria,pageableRequest)
+                .stream()
+                .map(MyExperienceResponseModel::of)
+                .collect(Collectors.toList());
+        var payload = convertSimplePageableResponse(experiences,pageableRequest);
+        return payload;
     }
 
     @Override
     public SimplePageableResponse<AnyExperienceResponseModel> allAnyExperiences(UUID targetResumeId, SimplePageableRequest pageableRequest) {
-        return null;
+        var criteria = ExperienceCriteria.Builder.builder().resumeID(ResumeID.of(targetResumeId)).accessModifiers(mockAccessModifiers).build();
+        var experiences = abstractExperienceQueryHandler.fetchAllExperiences(criteria,pageableRequest)
+                .stream()
+                .map(AnyExperienceResponseModel::of)
+                .collect(Collectors.toList());
+        var payload =  convertSimplePageableResponse(experiences,pageableRequest);
+        return payload;
     }
 
     @Override
-    public MyExperienceResponseModel myExperienceById(UUID id) {
-        return null;
+    public MyExperienceResponseModel myExperienceById(UUID id){
+        var resumeId = securityContextHolder.availableResumeID();
+        var criteria = CourseCriteria.Builder.builder().id(CourseID.of(id)).resumeID(resumeId).build();
+        var experience = courseQueryRepositoryAdapter.fetchCourseById(criteria);
+        var payload =  MyCourseResponseModel.of(experience.orElseThrow(ExperienceNotFound::new));
+        return payload;
     }
 
     @Override
@@ -44,5 +81,11 @@ public class ExperienceQueryHandler implements AbstractExperienceQueryHandler {
     @Override
     public AnyExperienceResponseModel anyExperienceById(UUID id) {
         return null;
+    }
+
+    private SimplePageableResponse convertSimplePageableResponse(List courses,SimplePageableRequest simplePageableRequest){
+        Boolean hasMore = courses.size()  > simplePageableRequest.getSize();
+        if(hasMore)courses.remove(courses.size()-1);
+        return SimplePageableResponse.of(simplePageableRequest.getSize(), simplePageableRequest.getPage(),hasMore,courses);
     }
 }
