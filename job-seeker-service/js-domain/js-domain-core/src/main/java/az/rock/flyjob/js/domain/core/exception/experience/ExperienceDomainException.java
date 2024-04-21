d83e284410ed6587a44609d3282397c278a6e408
metin <matinga@code.edu.az>
2024-04-21 16:47:14 +0400

    ExperienceCommandHandler finished

--- job-seeker-service/js-domain/js-domain-core/src/main/java/az/rock/flyjob/js/domain/core/exception/experience/ExperienceDomainException.java
+++ job-seeker-service/js-domain/js-domain-core/src/main/java/az/rock/flyjob/js/domain/core/exception/experience/ExperienceDomainException.java
@@ -3,21 +3,12 @@ package az.rock.flyjob.js.domain.core.exception.experience;
 import az.rock.flyjob.js.domain.core.exception.ResumeDomainException;
 
 public class ExperienceDomainException extends ResumeDomainException {
-<<<<<<< HEAD
-    public ExperienceDomainException() {
-        super("F0000000023");
-    }
 
     public ExperienceDomainException(String message) {
         super(message);
     }
-=======
-    public ExperienceDomainException(String message) {
-        super(message);
-    }
-
     public ExperienceDomainException() {
         super("F0000000001");
     }
->>>>>>> feature/js-develop/experience
+
 }
