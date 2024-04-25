49093aecb2a0268ebd09c0e236229bb40279c21d
metin <matinga@code.edu.az>
2024-04-26 01:23:47 +0400

    Project Update method

--- job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/adapter/query/ProjectQueryRepositoryAdapter.java
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/adapter/query/ProjectQueryRepositoryAdapter.java
@@ -1,5 +1,7 @@
 package az.rock.flyjob.js.dataaccess.adapter.query;
 
+import az.rock.flyjob.js.dataaccess.mapper.abstracts.AbstractProjectDataAccessMapper;
+import az.rock.flyjob.js.dataaccess.repository.abstracts.query.jpa.AbstractProjectQueryJPARepository;
 import az.rock.flyjob.js.domain.core.root.detail.ProjectRoot;
 import az.rock.flyjob.js.domain.presentation.ports.output.repository.query.AbstractProjectQueryRepositoryAdapter;
 import az.rock.lib.domain.id.js.ResumeID;
@@ -8,11 +10,23 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Component
 public class ProjectQueryRepositoryAdapter implements AbstractProjectQueryRepositoryAdapter {
+    private final AbstractProjectQueryJPARepository projectQueryJPARepository;
+    private final AbstractProjectDataAccessMapper projectDataAccessMapper;
+
+    public ProjectQueryRepositoryAdapter(AbstractProjectQueryJPARepository projectQueryJPARepository
+            , AbstractProjectDataAccessMapper projectDataAccessMapper) {
+        this.projectQueryJPARepository = projectQueryJPARepository;
+        this.projectDataAccessMapper = projectDataAccessMapper;
+    }
+
     @Override
     public Optional<ProjectRoot> findByResumeAndUuidAndRowStatusTrue(ResumeID resumeID, UUID projectId) {
-        return Optional.empty();
+        var entity = this.projectQueryJPARepository.findByIdAndResumeIdAndRowStatusActive(resumeID.getAbsoluteID(), projectId);
+        if (entity.isEmpty()) return Optional.empty();
+        return this.projectDataAccessMapper.toRoot(entity.get());
     }
 
     @Override
--- job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/repository/abstracts/query/jpa/AbstractProjectQueryJPARepository.java
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/repository/abstracts/query/jpa/AbstractProjectQueryJPARepository.java
@@ -1,11 +1,24 @@
 package az.rock.flyjob.js.dataaccess.repository.abstracts.query.jpa;
 
+import az.rock.flyjob.js.dataaccess.model.entity.resume.details.EducationEntity;
 import az.rock.flyjob.js.dataaccess.model.entity.resume.details.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AbstractProjectQueryJPARepository extends JpaRepository<ProjectEntity, UUID> {
+    @Query("SELECT e FROM ProjectEntity e WHERE e.resume.uuid = :resumeID AND e.uuid = :projectId AND e.rowStatus = 'ACTIVE'")
+    Optional<ProjectEntity> findByIdAndResumeIdAndRowStatusActive(@Param("resumeID") UUID resumeID, @Param("projectId") UUID projectId);
+
+    @Query("SELECT e FROM ProjectEntity e WHERE e.uuid = :projectId AND e.rowStatus = 'ACTIVE'")
+    Optional<ProjectEntity> findById(@Param("projectId") UUID projectId);
+
+    @Query("SELECT e FROM ProjectEntity e WHERE e.resume.uuid = :resumeID AND e.rowStatus = 'ACTIVE' order by e.orderNumber")
+    List<ProjectEntity> findAll(@Param("resumeID") UUID resumeID);
 }
