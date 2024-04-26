a7b923b821db2f592c377646d06e15bc01330887
metin <matinga@code.edu.az>
2024-04-26 12:11:37 +0400

    Reorder and Delete Method

--- job-seeker-service/js-application/js-spec/src/main/java/az/rock/flyjob/js/spec/privates/query/resume/detail/ProjectQueryPrivateSpec.java
+++ job-seeker-service/js-application/js-spec/src/main/java/az/rock/flyjob/js/spec/privates/query/resume/detail/ProjectQueryPrivateSpec.java
@@ -1,10 +1,25 @@
 package az.rock.flyjob.js.spec.privates.query.resume.detail;
 
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.contact.AnyContactResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.contact.MyContactResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.project.AnyProjectResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.project.MyProjectResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.project.simple.SimpleAnyProjectResponseModel;
+import az.rock.flyjob.js.domain.presentation.dto.response.resume.project.simple.SimpleMyProjectResponseModel;
 import az.rock.lib.jresponse.response.success.JSuccessDataResponse;
+import az.rock.lib.valueObject.SimplePageableRequest;
 import az.rock.lib.valueObject.SimplePageableResponse;
 import org.springframework.http.ResponseEntity;
 
+import java.util.UUID;
+
 public interface ProjectQueryPrivateSpec {
-    ResponseEntity<JSuccessDataResponse<SimplePageableResponse<>>>
+    ResponseEntity<JSuccessDataResponse<SimplePageableResponse<MyProjectResponseModel>>> queryAllMyProjects(SimplePageableRequest pageableRequest);
+    ResponseEntity<JSuccessDataResponse<SimplePageableResponse<AnyProjectResponseModel>>> queryAllAnyProjects(UUID targetResumeId, SimplePageableRequest pageableRequest);
+    ResponseEntity<JSuccessDataResponse<SimplePageableResponse<SimpleMyProjectResponseModel>>> queryAllMySimpleProjects(SimplePageableRequest pageableRequest);
+    ResponseEntity<JSuccessDataResponse<SimplePageableResponse<SimpleAnyProjectResponseModel>>> queryAllAnySimpleProjects(UUID targetResumeId, SimplePageableRequest pageableRequest);
+    ResponseEntity<JSuccessDataResponse<MyProjectResponseModel>> findMyProjectById(UUID id);
+
+    ResponseEntity<JSuccessDataResponse<AnyProjectResponseModel>> findAnyProjectById(UUID id);
 
 }
