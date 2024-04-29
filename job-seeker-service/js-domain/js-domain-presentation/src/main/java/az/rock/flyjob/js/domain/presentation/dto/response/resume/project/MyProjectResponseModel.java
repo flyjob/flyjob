764e4f2641b5204b77cfef42b12ecd54ab18945c
metin <matinga@code.edu.az>
2024-04-29 10:55:30 +0400

    Reorder and Delete Method

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/dto/response/resume/project/MyProjectResponseModel.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/dto/response/resume/project/MyProjectResponseModel.java
@@ -1,4 +1,31 @@
 package az.rock.flyjob.js.domain.presentation.dto.response.resume.project;
 
-public class MyProjectResponseModel {
+import az.rock.flyjob.js.domain.core.root.detail.ProjectRoot;
+import az.rock.flyjob.js.domain.presentation.dto.response.MyBaseResponseModel;
+import az.rock.lib.valueObject.AccessModifier;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@SuperBuilder
+public class MyProjectResponseModel extends MyBaseResponseModel {
+    private AccessModifier accessModifier;
+    private Integer orderNumber;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String description;
+    private String link;
+
+    public MyProjectResponseModel of(ProjectRoot root) {
+        return MyProjectResponseModel.builder()
+                .id(root.getRootID().getAbsoluteID())
+                .accessModifier(root.getAccessModifier())
+                .link(root.getLink())
+                .orderNumber(root.getOrderNumber())
+                .startDate(root.getStartDate())
+                .description(root.getDescription())
+                .build();
+    }
 }
